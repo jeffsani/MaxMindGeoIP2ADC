3d4100537befe934e1f03bcb650d7e4e5880d0e4
Jeff Sani <jeff.sani@gmail.com>
2022-03-29 14:28:14 -0700

    ugh

--- geolite2adc.sh
+++ geolite2adc.sh
@@ -4,7 +4,7 @@
 #set -e
 set -u
 set -o pipefail
-#set -x
+set -x
 
 # Variables to put into environment variables rather than leaving in the script
 LICENSE_KEY="141nr9qnsbEnkATO"
@@ -102,11 +102,11 @@ if [[ "$CHECKSUM" == "OK" ]]; then #convert and transfer file to ADC
    Citrix_Netscaler_InBuilt_GeoIP_DB_IPv6_B64=$(base64 -w0 Citrix_Netscaler_InBuilt_GeoIP_DB_IPv6);
    # Copy the files to ADC
    STATUS_CODE=$(curl -s -k  -w "%{http_code}\n" -X POST -H "Accept: application/json" -H "Content-Type: application/vnd.com.citrix.netscaler.systemfile+json" -H "Authorization: Basic $(echo -n ${CITRIX_ADC_USER}:${CITRIX_ADC_PASSWORD} | base64)" "https://${CITRIX_ADC_IP}/nitro/v1/config/systemfile" -d '{"systemfile":{"filename":"Citrix_Netscaler_InBuilt_GeoIP_DB_IPv4","filelocation":"${CITRIX_ADC_GEOIPDB_PATH}","filecontent":"${Citrix_Netscaler_InBuilt_GeoIP_DB_IPv4_B64}","fileencoding":"BASE64"}}');
-   if [[ "$STATUS_CODE" -ne 200 ]] ; then
+   if [[ "$STATUS_CODE" -ne 200 ]]; then
       echo "Citrix ADC Nitro API returned HTTP Status Code $STATUS_CODE..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
    fi
    STATUS_CODE=$(curl -s -k -w "%{http_code}\n" -X POST -H "Accept: application/json" -H "Content-Type: application/vnd.com.citrix.netscaler.systemfile+json" -H "Authorization: Basic $(echo -n ${CITRIX_ADC_USER}:${CITRIX_ADC_PASSWORD} | base64)" "https://${CITRIX_ADC_IP}/nitro/v1/config/systemfile" -d '{"systemfile":{"filename":"Citrix_Netscaler_InBuilt_GeoIP_DB_IPv6","filelocation":"${CITRIX_ADC_GEOIPDB_PATH}","filecontent":"${Citrix_Netscaler_InBuilt_GeoIP_DB_IPv6_B64}","fileencoding":"BASE64"}}');
-   if [[ "$STATUS_CODE" -ne 200 ]] ; then
+   if [[ "$STATUS_CODE" -ne 200 ]]; then
       echo "Citrix ADC Nitro API returned HTTP Status Code $STATUS_CODE..." | ts '[%H:%M:%S]' | tee -a $LOGFILE;
    fi
 else
