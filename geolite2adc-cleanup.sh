70d499a6db8e79c922d551d698af344a350d20c1
Jeff Sani <jeff.sani@gmail.com>
2022-04-25 08:09:04 -0700

    deletion

--- geolite2adc-cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# geolite2adc-cleanup.sh
-# This script will remove old logs so they do not consume your disk
-# See geoliteadc-init.sh for more information on scheduled cron task
-
-find *.log -type f -not -name '*maxmindgeolite2adc-init.log'-delete
